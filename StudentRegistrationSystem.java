1f4cadca5d73591fcd6050e67361773f6ab07304
MONIKA2890 <monika.sharma_cs24@gla.ac.in>
2025-09-01 16:23:33 +0530

    Student registration completed

---
+++ StudentRegistrationSystem.java
@@ -0,0 +1,16 @@
+import java.util;
+public class StudentRegistrationSystem {
+ public static void Students(String[] Students) {
+    System.out.println("Enter Student Name:");
+ }
+
+    
+    public static void CompareString(String str1,String str2) {
+     String Str1="ABC";
+     String Str2="DEF"; 
+     String Str3= new String("NMN");
+     System.out.println(Str1==Str2);
+     System.out.println(Str1.equals(Str2));
+
+    }
+}
